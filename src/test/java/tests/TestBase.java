de7e8b74f95e31765491ec2e074bbdaf1eaffe52
KMitrofanova <vip.ksundra@mail.ru>
2024-01-11 20:57:15 +0300

    added allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,15 +1,22 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+
 import org.junit.jupiter.api.BeforeAll;
+
+
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll(){
+
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
     }
 }
