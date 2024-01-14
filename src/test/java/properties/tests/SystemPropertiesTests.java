06ef27cf620f967b4f051606267e18498cbb6055
KMitrofanova <vip.ksundra@mail.ru>
2024-01-14 20:22:56 +0300

    added property

---
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -0,0 +1,40 @@
+package properties.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+    @Test
+    void systemPropertiesTest(){
+        String browser = System.getProperty("browse");
+        System.out.println(browser);
+    }
+
+    @Test
+    void systemProperties1Test(){
+        System.setProperty("browser","chrome");
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+
+    @Test
+    void systemProperties2Test(){
+        String browser = System.getProperty("browser","mozilla");
+        System.out.println(browser);
+    }
+
+    @Test
+    void systemProperties3Test(){
+        System.setProperty("browser","chrome");
+        String browser = System.getProperty("browser","mozilla");
+        System.out.println(browser);
+    }
+
+    @Test
+    @Tag("property")
+    void systemProperties4Test(){
+        String browser = System.getProperty("browser","mozilla");
+        System.out.println(browser);
+
+    }
+}
