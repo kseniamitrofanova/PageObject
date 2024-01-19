a5792d31dbe7e00b161904ad79e29b3edf321078
KMitrofanova <vip.ksundra@mail.ru>
2024-01-19 21:50:53 +0300

    updated

--- src/test/java/pages/StudentRegistrationPage.java
+++ src/test/java/pages/StudentRegistrationPage.java
@@ -15,7 +15,7 @@ public class StudentRegistrationPage {
             userNumberInput =$("#userNumber"),
             genderWrapperInput = $("#genterWrapper"),
             calendarInput = $("#dateOfBirthInput"),
-            buttonSubmit= $("#submit.btn.btn-primary"),
+            buttonSubmit= $("#submit"),
             hobbiesWrapperInput = $("#hobbiesWrapper"),
             subjectsInput= $("#subjectsInput"),
             loadPicture=$("#uploadPicture"),
